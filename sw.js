69eeb66acf78dab6e15c9329ccd1e94e90e9e515
vikingcat77 <a24sergiopl@iessanclemente.net>
2026-06-11 19:38:54 +0200

    Prepara app como PWA instalable

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "divisiones-pwa-v1";
+
+const APP_ASSETS = [
+  "./",
+  "./index.html",
+  "./divisiones.html",
+  "./artediv.css",
+  "./divi.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/favicon-32.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
