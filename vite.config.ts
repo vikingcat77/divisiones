b37678e25dcfd4afc1e34a52c78e1543d354fee7
vikingcat77 <a24sergiopl@iessanclemente.net>
2026-07-01 21:43:05 +0200

    Arregla publicacion en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/divisiones/' : '/',
+  base: command === 'build' ? './' : '/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 }));
